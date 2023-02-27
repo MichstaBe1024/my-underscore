382d6981b22d3b476f1e42a00e0d2e4bdb072222
MichstaBe <2318113474@qq.com>
2023-02-27 17:55:04 +0800

    feat:  实现each

--- src/index.js
+++ src/index.js
@@ -1,16 +1,163 @@
-const _ = require('underscore');
-const { each } = require('./each')
+(function () {
+  var root = (typeof self === 'object' && self.self === self && self) || (typeof global === 'object' && global.global === global && global) || this || {};
 
-const arr = [1, 3, 4]
-const obj = { one: 1, two: 3, there: 4 }
+  var ArrayProto = Array.prototype, ObjProto = Object.prototype;
+  var SymbolProto = typeof Symbol !== 'undefined' ? Symbol.prototype : null;
 
-const result = _.each(arr, (value) => {
-  return value * 2
-})
+  var push = ArrayProto.push,
+    slice = ArrayProto.slice,
+    toString = ObjProto.toString,
+    hasOwnProperty = ObjProto.hasOwnProperty;
 
-const result1 = each(arr, (value) => {
-  console.log('myEach', value);
-  return value * 2;
-})
-console.log('result1', result1)
-console.log('result', result)
+  var nativeIsArray = Array.isArray,
+    nativeKeys = Object.keys,
+    nativeCreate = Object.create;
+
+  var _ = function (obj) {
+    if (!(this instanceof _)) return new _(obj);
+    this._wrapped = obj;
+  };
+
+  if (typeof exports !== 'undefined' && !exports.nodeType) {
+    if (typeof module !== 'undefined' && !module.nodeType && module.exports) {
+      exports = module.exports = _;
+    }
+    exports._ = _;
+  } else {
+    root._ = _;
+  }
+
+  _.VERSION = '1.0.0';
+
+  var optimizeCb = function (func, context, argCount) {
+    if (context === void 0) return func;
+    switch (argCount === null ? 3 : argCount) {
+      case 1: return function (value) {
+        return func.call(context, value);
+      };
+      case 2: return function (value, index, collection) {
+        return func.call(context, value, index, collection);
+      };
+      case 3: return function (accumulator, value, index, collection) {
+        return func.call(context, accumulator, value, index, collection);
+      };
+    }
+    return function () {
+      return func.apply(context, arguments);
+    };
+  };
+
+  var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+
+  // 类数组
+  var isArrayLike = function (collection) {
+    var length = collection.length;
+    return typeof length === 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+  };
+
+  var has = function (obj, path) {
+    return obj !== null && hasOwnProperty.call(obj, path);
+  };
+
+  var hasEnumBug = !{ toString: null }.propertyIsEnumerable('toString');
+  var nonEnumerableProps = ['valueOf', 'isPrototypeOf', 'toString',
+    'propertyIsEnumerable', 'hasOwnProperty', 'toLocaleString'];
+
+  var collectNonEnumProps = function (obj, keys) {
+    var nonEnumIdx = nonEnumerableProps.length;
+    var constructor = obj.constructor;
+    var proto = _.isFunction(constructor) && constructor.prototype || ObjProto;
+
+    // Constructor is a special case.
+    var prop = 'constructor';
+    if (has(obj, prop) && !_.contains(keys, prop)) keys.push(prop);
+
+    while (nonEnumIdx--) {
+      prop = nonEnumerableProps[nonEnumIdx];
+      if (prop in obj && obj[prop] !== proto[prop] && !_.contains(keys, prop)) {
+        keys.push(prop);
+      }
+    }
+  };
+
+  _.contains = _.inculde = function (obj, item, fromIndex, guard) {
+    if (!isArrayLike) return obj = _.values(obj);
+  };
+
+  _.each = _.forEach = function (obj, iteratee, context) {
+    iteratee = optimizeCb(iteratee, context);
+    var length, i;
+    if (isArrayLike(obj)) {
+      for (i = 0, length = obj.length; i < length; i++) {
+        iteratee(obj[i], i, obj);
+      }
+    } else {
+      var keys = _.keys(obj);
+      for (i = 0, length = keys.length; i < length; i++) {
+        iteratee(obj[keys[i]], keys[i], obj);
+      }
+    }
+    return obj;
+  };
+
+  _.isFunction = function (obj) {
+    return typeof obj === 'function' || false;
+  };
+
+  _.keys = function (obj) {
+    if (!_.isObject(obj)) return [];
+    if (nativeKeys) return nativeKeys(obj);
+    var keys = [];
+    for (var key in obj) if (has(obj, keys)) keys.push(key);
+    // Ahem, IE < 9.
+    if (hasEnumBug) collectNonEnumProps(obj, keys);
+    return keys;
+  };
+
+  _.values = function (obj) {
+    var keys = _.keys(obj);
+    var length = keys.length;
+    var values = Array(length);
+    for (var i = 0; i < length; i++) {
+      values[i] = obj[keys[i]];
+    }
+    return values;
+  };
+
+  // 判断是否是对象，包含数组对象与函数对象
+  _.isObject = function (obj) {
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  };
+
+  _.functions = function (obj) {
+    var names = [];
+    for (var key in obj) {
+      if (_.isFunction(obj[key])) names.push(key);
+    }
+    return names.sort();
+  };
+
+  var chainResult = function (instance, obj) {
+    return instance._chain ? _(obj).chain() : obj;
+  }
+
+  _.mixin = function (obj) {
+    _.each(_.functions(obj), function (name) {
+      var func = _[name] = obj[name];
+      _.prototype[name] = function () {
+        var args = [this._wrapped];
+        push.apply(args, arguments);
+        return chainResult(this, func.apply(_, args));
+      }
+    });
+    return _;
+  };
+
+  // Add all of the Underscore functions to the wrapper object.
+  _.mixin(_);
+
+  _.prototype.value = function () {
+    return this._wrapped;
+  };
+}());
